aceed08e876111e3e2882ce1d5b390b54479e5ba
Nux007 <007.nux@gmail.com>
2021-02-21 10:46:20 +0100

    Exo 191.

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * Reprenez le code de l'exercice précédent et transformez vos requêtes pour utiliser les requêtes préparées
+ * la méthode de bind du paramètre et du choix du marqueur de données est à votre convenance.
+ */
